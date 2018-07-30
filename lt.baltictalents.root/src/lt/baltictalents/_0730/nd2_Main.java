2fbab9c64008a41a648acb5822a53ca8c439b39e
Paulius Kazokas <BTpauliuskazokas@gmail.com>
2018-07-30 16:34:56 +0300

    nd2_0730 Optimized

--- lt.baltictalents.root/src/lt/baltictalents/_0730/nd2_Main.java
+++ lt.baltictalents.root/src/lt/baltictalents/_0730/nd2_Main.java
@@ -3,13 +3,14 @@ package lt.baltictalents._0730;
 public class nd2_Main {
     public static void main(String[] args){
         int count = 0; int total = 0;
+        System.out.print("Paskutiniu skaiciu: ");
         for (int i = 100; i >= 1; i--){
-            System.out.println(i + " ");
             if (count < 3){
                 count++;
                 total += i;
+                System.out.print(i + " ");
             }
         }
-        System.out.println("Suma: " + total);
+        System.out.print("suma yra " + total);
     }
 }
