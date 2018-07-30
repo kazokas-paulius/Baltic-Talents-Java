26abac2237d4229e8782ea5909258f965d464eee
Paulius Kazokas <BTpauliuskazokas@gmail.com>
2018-07-30 16:24:38 +0300

    nd1_0730 Completed

--- lt.baltictalents.root/src/lt/baltictalents/_0730/nd1_Main.java
+++ lt.baltictalents.root/src/lt/baltictalents/_0730/nd1_Main.java
@@ -2,12 +2,14 @@ package lt.baltictalents._0730;
 // Susumuoti pirmus (mažiausius) penkis nelyginius skaičius iš intervalo [1; 100].
 public class nd1_Main {
     public static void main(String[] args){
-        int count = 0;
+        int count = 0; int total = 0;
         for (int i = 1; i < 100; i++){
             if (i % 2 != 0 && count < 5){
                 count++;
+                total += i;
                 System.out.print(i + " ");
             }
         }
+        System.out.println("| Suma: " + total);
     }
 }
---
+++ lt.baltictalents.root/src/lt/baltictalents/_0730/nd2_Main.java
@@ -0,0 +1,5 @@
+package lt.baltictalents._0730;
+
+public class nd2_Main {
+
+}
