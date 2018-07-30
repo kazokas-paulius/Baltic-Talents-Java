5d367f6d3f1f3e384679eada98e69b358758fd9b
Paulius Kazokas <BTpauliuskazokas@gmail.com>
2018-07-30 16:32:05 +0300

    nd2_0730 Quick bugfix

--- lt.baltictalents.root/src/lt/baltictalents/_0730/nd2_Main.java
+++ lt.baltictalents.root/src/lt/baltictalents/_0730/nd2_Main.java
@@ -2,14 +2,14 @@ package lt.baltictalents._0730;
 // Susumuoti paskutinius (didžiausius) tris skaičius iš intervalo [1; 100].
 public class nd2_Main {
     public static void main(String[] args){
-        int count = 0; int suma = 0;
+        int count = 0; int total = 0;
         for (int i = 100; i >= 1; i--){
             System.out.println(i + " ");
             if (count < 3){
                 count++;
-                suma += i;
+                total += i;
             }
         }
-        System.out.println(suma);
+        System.out.println("Suma: " + total);
     }
 }
