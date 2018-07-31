ed4621172ceec5ea2d5fcfbb44bc57392e0c8e05
Paulius Kazokas <BTpauliuskazokas@gmail.com>
2018-07-31 11:30:26 +0300

    nd_0730 Optimized

--- lt.baltictalents.root/src/lt/baltictalents/_0730/nd1_Main.java
+++ lt.baltictalents.root/src/lt/baltictalents/_0730/nd1_Main.java
@@ -4,10 +4,13 @@ public class nd1_Main {
     public static void main(String[] args){
         int count = 0; int total = 0;
         for (int i = 1; i < 100; i++){
-            if (i % 2 != 0 && count < 5){
+            if (i % 2 != 0){
                 count++;
                 total += i;
                 System.out.print(i + " ");
+            }
+            if(count == 5){
+                break;
             }
         }
         System.out.println("| Suma: " + total);
--- lt.baltictalents.root/src/lt/baltictalents/_0730/nd2_Main.java
+++ lt.baltictalents.root/src/lt/baltictalents/_0730/nd2_Main.java
@@ -5,11 +5,13 @@ public class nd2_Main {
         int count = 0; int total = 0;
         System.out.print("Paskutiniu skaiciu: ");
         for (int i = 100; i >= 1; i--){
-            if (count < 3){
-                count++;
-                total += i;
-                System.out.print(i + " ");
+            if (count == 3){
+                break;
             }
+            count++;
+            total += i;
+//            if(true){continue;}
+            System.out.print(i + " ");
         }
         System.out.print("suma yra " + total);
     }
