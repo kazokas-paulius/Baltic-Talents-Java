397582098576d84be132051da1107a5477f0db35
Paulius Kazokas <BTpauliuskazokas@gmail.com>
2018-08-02 11:20:42 +0300

    minor nd1_Main changes

--- lt.baltictalents.root/src/lt/baltictalents/_0731/ndBONUS_Main.java
+++ lt.baltictalents.root/src/lt/baltictalents/_0731/ndBONUS_Main.java
@@ -5,7 +5,7 @@ public class ndBONUS_Main {
         int min = 1; int max = 1000;
         for(min = 1; min <= max; min++){
             int sum = 0;
-            for(int j = 1; j < min; j++){
+            for(int j = 1; j < min; j++){ // forins range didinant min reiksme (min 10 tada forins iki 10)
                 if((min % j) == 0){
                     sum += j;
                 }
