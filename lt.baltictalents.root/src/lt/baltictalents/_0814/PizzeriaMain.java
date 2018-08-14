5fb8a849639f58ae141278b3aaa3d6e7d97ca009
Paulius Kazokas <BTpauliuskazokas@gmail.com>
2018-08-14 14:38:48 +0300

    Pizzeria

---
+++ lt.baltictalents.root/src/lt/baltictalents/_0814/PizzeriaMain.java
@@ -0,0 +1,7 @@
+package lt.baltictalents._0814;
+
+public class PizzeriaMain {
+    public static void main(String[] args) {
+
+    }
+}
--- lt.baltictalents.root/src/lt/baltictalents/_test/test1.java
+++ lt.baltictalents.root/src/lt/baltictalents/_test/test1.java
@@ -2,9 +2,7 @@ package lt.baltictalents._test;
 
 public class test1 {
     public static void main(String[] args){
-        int a = 100;
-        int b = a;
-        a = 200;
-        System.out.println(b);
+        int n = 1;
+        System.out.println("Du " + args[n]);
     }
 }
