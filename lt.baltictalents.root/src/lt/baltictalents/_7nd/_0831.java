7c502886cc7c60717d4a1a044f9d5a8076eb789c
Paulius Kazokas <BTpauliuskazokas@gmail.com>
2018-09-07 11:15:59 +0300

    _0831 nd

--- lt.baltictalents.root/src/lt/baltictalents/_7nd/_0831.java
+++ lt.baltictalents.root/src/lt/baltictalents/_7nd/_0831.java
@@ -5,50 +5,16 @@ import java.util.*;
 import java.util.Map.Entry;
 
 public class _0831 {
-    public final static String PATH = "resources/wiki";
-    public static File wikiFile = new File(PATH);
+    private final static String PATH = "resources/wiki";
+    private static File wikiFile = new File(PATH);
 
     public static void main(String[] args) throws IOException {
-
-        Scanner sc = new Scanner(new File(PATH));
-        List<String> list = new ArrayList<>();
-
-        while (sc.hasNext()) {
-            list.add(sc.next());
-        }
-
-        sc.close();
-
-        String[] stArr = new String[list.size()];
-
-        Map<String, Integer> map = new TreeMap<>();
-        for (int i = 0; i < list.size(); i++) {
-            stArr[i] = list.get(i).replaceAll("([,.;:\"'-()\\s])", "");
-        }
-
-        for (int i = 0; i < stArr.length; i++) {
-            int cunt = 1;
-            for(int j =0; j < stArr.length; j++){
-                String newWord = stArr[i].toLowerCase();
-                if(newWord.compareToIgnoreCase(stArr[j]) == 0){
-                    map.put(newWord, cunt++);
-                }
-            }
-        }
-
-        Map<String, Integer> sortedByQuatiti = sortByValue(map);
-
-        for(Map.Entry<String, Integer> entry : sortedByQuatiti.entrySet()){
-            System.out.println("\"" + entry.getKey() + "\"" + " used: " + entry.getValue() + " times.");
-        }
-
-        wordCounter();
+        wordPopulation();
     }
 
     private static <K, V> Map<K, V> sortByValue(Map<K, V> map) {   // Isidedam metoda kuris komparuoja mapa pagal reiksmes.
         List<Entry<K, V>> list = new LinkedList<>(map.entrySet());
         Collections.sort(list, new Comparator<Object>() {
-            @SuppressWarnings("unchecked")
             public int compare(Object o1, Object o2) {
                 return ((Comparable<V>) ((Map.Entry<K, V>) (o2)).getValue())
                         .compareTo(((Map.Entry<K, V>) (o1)).getValue());
@@ -65,16 +31,52 @@ public class _0831 {
     }
 
     private static void wordCounter() {
-        try (Scanner sc = new Scanner(new FileInputStream(wikiFile))) {
+        try (Scanner scanner = new Scanner(new FileInputStream(wikiFile))) {
             int count = 0;
-            while (sc.hasNext()) {
-                sc.next();
+            while (scanner.hasNext()) {
+                scanner.next();
                 count++;
             }
             System.out.println("Number of words: " + count);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void wordPopulation() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(PATH));
+        List<String> list = new ArrayList<>();
+
+        while (scanner.hasNext()) {
+            list.add(scanner.next());
+        }
+
+        scanner.close();
+
+        String[] stringArray = new String[list.size()];
+
+        Map<String, Integer> map = new TreeMap<>();
+        for (int i = 0; i < list.size(); i++) {
+            stringArray[i] = list.get(i).replaceAll("([,.;:\"'-()\\s])", "");
+        }
+
+        for (int i = 0; i < stringArray.length; i++) {
+            int count = 1;
+            for(int j =0; j < stringArray.length; j++){
+                String newWord = stringArray[i].toLowerCase();
+                if(newWord.compareToIgnoreCase(stringArray[j]) == 0){
+                    map.put(newWord, count++);
+                }
+            }
+        }
+
+        Map<String, Integer> sortedByQuatity = sortByValue(map);
+
+        for(Map.Entry<String, Integer> entry : sortedByQuatity.entrySet()){
+            System.out.println("\"" + entry.getKey() + "\"" + " used: " + entry.getValue() + " times.");
+        }
+
+        wordCounter();
     }
 
 }
